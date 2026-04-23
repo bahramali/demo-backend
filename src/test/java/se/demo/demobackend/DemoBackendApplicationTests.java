80cd6c8881cc4dafeb6f2b321d79cb2a5c5ea284
bahramali <bahramali.az@gmail.com>
2026-04-23 06:19:04 +0200

    init structure

---
+++ src/test/java/se/demo/demobackend/DemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.demo.demobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
